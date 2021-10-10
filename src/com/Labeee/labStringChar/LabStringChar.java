4ebceabf4e596ab1df24d075185ddcee4a2ccf53
Suparoek <39317424+Suparoek@users.noreply.github.com>
2021-10-10 13:40:54 +0700

    labchar

---
+++ src/com/Labeee/labStringChar/LabStringChar.java
@@ -0,0 +1,10 @@
+package com.Labeee.labStringChar;
+
+public class LabStringChar {
+	public static void main(String[] arges) {
+		char[] myChar = {'J','o','i','n','E','m','p','l','o','e','e'};
+		String mystring = new String(myChar);
+		System.out.println(mystring);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module com.Labeee.labStringChar {
+}
